5434e1d03c87658f2bf05b10c9147410e94868cc
Colin McGraw <Colin.McGraw@GMail.com>
2020-05-03 19:27:11 -0500

    Added FAQ page.

--- ImmuneAndReadyToHelp/Pages/Faq.cshtml.cs
+++ ImmuneAndReadyToHelp/Pages/Faq.cshtml.cs
@@ -2,15 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ImmuneAndReadyToHelp.Core.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace ImmuneAndReadyToHelp.Pages
 {
     public class FaqModel : PageModel
     {
-        public void OnGet()
+        public Coordinate AustinDefaultLocation { get; } = new Coordinate { Latitude = 30.2672, Longitude = -97.7431 };
+
+        public string GoogleMapsApiKey { get; set; }
+
+        public FaqModel(IConfiguration config)
         {
+            GoogleMapsApiKey = config["GoogleMapsApiKey"];
         }
     }
 }
